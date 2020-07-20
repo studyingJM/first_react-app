1a903626484c9ff3c9137c4940cb786572d38624
Jimin <kjimin2123@gmail.com>
2020-07-20 11:46:01 +0900

    State

--- src/App.js
+++ src/App.js
@@ -5,13 +5,34 @@ import Content from "./components/Content.js";
 import './App.css';
 
 class App extends Component {
+  //stat값을 초기화 시켜주는 작업.
+  /*
+    불필요한 정보를 노출시키지 않게하여 가독성을 높임?
+    class가 내부적으로 사용할 상태는 state형태를 이용.
+    상위 컴포넌트의 state값을 하위 컴포넌트의 props로 전달.
+  */
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:"web", sub:"My first react and git"},
+      contents:[
+        {id:1,title:"HTML", desc:"HTML is HyperText ..."},
+        {id:2,title:"CSS", desc:"CSS is Design"},
+        {id:3,title:"JavaScript", desc:"JavaScript is for interactive"}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Sub title="WEB" sub="my first react and git"></Sub>
+        <Sub 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Sub>
         <Sub title="React" sub="react"></Sub>
         <Sub title="Git" sub="git"></Sub>
-        <Toc></Toc>
+        <Toc data={this.state.contents}></Toc>
         <Content title="HTML" desc="Hello World"></Content>
       </div>
     );
--- src/components/Toc.js
+++ src/components/Toc.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'; //React는 필수.
 
 class Toc extends Component {
     render() {
+      var list = [];
+      var data = this.props.data
+      var i = 0;
+      while(i < data.length) {
+      //자동 생성 항목들은 key라는 고유의 식별자가 필요하다.
+      list.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        i = i + 1;
+      } 
       return (
         <nav>
           <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
+            {list}
           </ul>
         </nav>
       );
