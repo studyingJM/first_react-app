faedc3c7fecb8b4152cfbd3baf24659e5e60b1f7
Jimin <kjimin2123@gmail.com>
2020-07-20 13:55:09 +0900

    modified event2

--- src/components/Toc.js
+++ src/components/Toc.js
@@ -11,12 +11,13 @@ class Toc extends Component {
         <li key={data[i].id}>
           <a 
             href = {"/content/"+data[i].id}
-            data-id = {data[i].id} //속성 target.dataset
-            onClick = {function(e) {
+            // data-id = {data[i].id} //속성 target.dataset
+            //속성을 이용하지 않는 방법.
+            onClick = {function(id,e) {
               e.preventDefault();
               //App.js에 onCHangePage() 함수 실행.
-              this.props.onChangePage(e.target.dataset.id);
-            }.bind(this)}
+              this.props.onChangePage(id);
+            }.bind(this,data[i].id)}
             >
             {data[i].title}
           </a>
